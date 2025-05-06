8ff5e5ffbd92f8d7f6b14d8ead4efc80a176b694
dyenchristelle <0324-0914@lspu.edu.ph>
2025-05-06 18:16:52 +0800

    try

--- src/main/java/com/website/demo/WebConfig.java
+++ src/main/java/com/website/demo/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("https://e0f4-136-158-65-43.ngrok-free.app")
+        .allowedOrigins("https://82af-136-158-65-43.ngrok-free.app")
         // .allowedOrigins("http://localhost:9090")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
